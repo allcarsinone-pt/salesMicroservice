76f4bf974bbf89f0f27c3ec4db2e751d3a288802
goncalos10 <hugo10simoes10@gmail.com>
2023-12-17 17:04:35 +0000

    added roles

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const PaymentMethodController = require('../controllers/PaymentMethodController');
 const PaymentController = require('../controllers/PaymentController');
 const AuthServiceMiddleware = require('../middlewares/AuthServiceMiddleware');
+const roles = require('../models/Roles');
 
-router.post('/createPaymentMethod', PaymentMethodController.createPaymentMethod);
-router.get('/getAllPaymentMethods', PaymentMethodController.getAllPaymentMethods);
-router.delete('/deletePaymentMethod/:id', PaymentMethodController.deletePaymentMethod);
+router.post('/createPaymentMethod', AuthServiceMiddleware.execute([roles.ADMIN]), PaymentMethodController.createPaymentMethod);
+router.get('/getAllPaymentMethods', AuthServiceMiddleware.execute([roles.ADMIN]), PaymentMethodController.getAllPaymentMethods);
+router.delete('/deletePaymentMethod/:id', AuthServiceMiddleware.execute([roles.ADMIN]), PaymentMethodController.deletePaymentMethod);
 router.post('/createPayment', AuthServiceMiddleware.execute, PaymentController.createPayment);
 router.get('/getAllPayments', AuthServiceMiddleware.execute ,PaymentController.getAllPaymentForUser);
 
