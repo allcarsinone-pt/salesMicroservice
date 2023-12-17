05346ee6742c9b5b8ee7f3a306c03bc6838601c8
goncalos10 <hugo10simoes10@gmail.com>
2023-12-17 10:29:55 +0000

    changed authservicemiddleware

--- src/middlewares/AuthServiceMiddleware.js
+++ src/middlewares/AuthServiceMiddleware.js
@@ -1,23 +1,26 @@
 class AuthServiceMiddleware {
+    
+
     static async execute(request, response, next) {
+        try {
         const authService = request.app.get('AuthAdapter')
         const logService = request.app.get('LogAdapter')
 
         const token = request.headers.authorization
         if(!token) {
-            await logService.execute('SalesService', 'Missing token', 'error')
+            await logService.execute('StandsService', 'Missing token', 'error')
             return response.status(401).json({ error: 'Missing token' })
         }
         const decodedToken = await authService.login(token)
-
-        console.log(decodedToken)
         if(decodedToken.status !== 200) {
-            await logService.execute('SalesService', 'Invalid token', 'error')
-            return response.status(401).json(decodedToken.body)
+            await logService.execute('StandsService', 'Invalid token', 'error')
+            return response.status(401).json(decodedToken.data)
         }
-        request.user = decodedToken
         next()
+        }
+        catch {
+          await logService.execute('StandsService', 'Token expired', 'error')
+          return res.status(401).json({message: "Token expired"})
+        }
     }
 }
-
-module.exports = AuthServiceMiddleware
