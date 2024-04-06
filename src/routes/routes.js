e0fb54e8b0acdd0f6733ebd98393c836b3dc3f0e
goncalos10 <a18847@alunos.ipca.pt>
2024-04-06 12:24:03 +0100

    chore: changed payments routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,10 +4,10 @@ const PaymentController = require('../controllers/PaymentController');
 const AuthServiceMiddleware = require('../middlewares/AuthServiceMiddleware');
 const roles = require('../models/Roles');
 
-router.post('/createPaymentMethod', AuthServiceMiddleware.execute([roles.ADMIN]), PaymentMethodController.createPaymentMethod);
-router.get('/getAllPaymentMethods', AuthServiceMiddleware.execute([roles.ADMIN]), PaymentMethodController.getAllPaymentMethods);
-router.delete('/deletePaymentMethod/:id', AuthServiceMiddleware.execute([roles.ADMIN]), PaymentMethodController.deletePaymentMethod);
-router.post('/createPayment', AuthServiceMiddleware.execute([]), PaymentController.createPayment);
-router.get('/getAllPayments', AuthServiceMiddleware.execute([]) ,PaymentController.getAllPaymentForUser);
+router.post('/methods/', AuthServiceMiddleware.execute([roles.ADMIN]), PaymentMethodController.createPaymentMethod);
+router.get('/methods', AuthServiceMiddleware.execute([roles.ADMIN]), PaymentMethodController.getAllPaymentMethods);
+router.delete('/methods/:id', AuthServiceMiddleware.execute([roles.ADMIN]), PaymentMethodController.deletePaymentMethod);
+router.post('/', AuthServiceMiddleware.execute([]), PaymentController.createPayment);
+router.get('/', AuthServiceMiddleware.execute([]) ,PaymentController.getAllPaymentForUser);
 
 module.exports = router;
